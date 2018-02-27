05b2e013632e808e334834f75ba073da00317b3d
Chelsea Conwell <cmconwel@my.loyno.edu>
2018-02-27 10:41:00 -0600

    Added Interactive Button
    Added a button to show more info

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
+var visibleText = "Show Less"
+var hiddenText = "Show More"
 $(document).ready(function () {
     alert("hello");
+    $(".info-box button").click(function () {
+        $(this).toggleClass("visible");
+        if ($(this).hasClass("visible")) {
+            $(this).html(hiddenText);
+        }
+        else {
+            $(this).html(visibleText);
+        }
+        $(this).next().toggleClass("visible");
+    });
 })
